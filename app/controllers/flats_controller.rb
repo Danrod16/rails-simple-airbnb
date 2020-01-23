7071a19a61bb58333bef51e45186ca04e140206f
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-01-23 12:18:59 +0100

    FEAT: Params added

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -14,4 +14,10 @@ class FlatsController < ApplicationController
 
   def destroy
   end
+
+private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
