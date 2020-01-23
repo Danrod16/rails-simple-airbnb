bbd22a16e8471d0bf949abba3aaf7b34491edb42
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-01-23 12:13:57 +0100

    FEAT: index created

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,17 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning DataBase..."
+Flat.destroy_all
+
+puts "creating Flats"
+Flat.create!(
+  name:'Awesome Flat with sun',
+  address:'plaza catalunya',
+  description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero cupiditate nesciunt atque id itaque quod, accusantium unde, ipsum sint aut eum nulla nisi! Dolor nulla dolorem eaque, reiciendis debitis illum.',
+  price_per_night: 70,
+  number_of_guests: 3
+  )
+
+Flat.create!(
+  name:'Awesome Flat with sun',
+  address:'plaza catalunya',
+  description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero cupiditate nesciunt atque id itaque quod, accusantium unde, ipsum sint aut eum nulla nisi! Dolor nulla dolorem eaque, reiciendis debitis illum.',
+  price_per_night: 70,
+  number_of_guests: 3
+  )
+puts "Flats #{Flat.count}created"
+
